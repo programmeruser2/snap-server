347ec7e34e99547d101102c5cdf83de9895bd134
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2021-03-01 15:34:13 -0500

    Replace tabs with spaces

--- server.js
+++ server.js
@@ -7,40 +7,40 @@ const server = http.createServer(app);
 app.use(express.static('snap'));
 const wss = new WebSocket.Server({ server });
 wss.on('connection', ws => {
-	const contextObject = {
-		AbortController,
-		AbortSignal,
-		Buffer,
-		__dirname,
-		__filename: '<sandbox>',
-		clearImmediate,
-		clearInterval,
-		clearTimeout,
-		console,
-		Event,
-		EventTarget,
-		exports,
-		global: Object.assign({}, global),
-		MessageChannel,
-		MessageEvent,
-		MessagePort,
-		process,
-		queueMicrotask,
-		require,
-		setImmediate,
-		setInterval,
-		setTimeout,
-		TextDecoder,
-		TextEncoder,
-		URL,
-		URLSearchParams,
-		WebAssembly
-	};
-	vm.createContext(contextObject);
-	ws.on('message', code => {
-		vm.runInContext(code, contextObject);
-	});
+    const contextObject = {
+        AbortController,
+        AbortSignal,
+        Buffer,
+        __dirname,
+        __filename: '<sandbox>',
+        clearImmediate,
+        clearInterval,
+        clearTimeout,
+        console,
+        Event,
+        EventTarget,
+        exports,
+        global: Object.assign({}, global),
+        MessageChannel,
+        MessageEvent,
+        MessagePort,
+        process,
+        queueMicrotask,
+        require,
+        setImmediate,
+        setInterval,
+        setTimeout,
+        TextDecoder,
+        TextEncoder,
+        URL,
+        URLSearchParams,
+        WebAssembly
+    };
+    vm.createContext(contextObject);
+    ws.on('message', code => {
+        vm.runInContext(code, contextObject);
+    });
 });
 server.listen(8080, () => {
-	console.log('Listening on port 8080');
+    console.log('Listening on port 8080');
 });
